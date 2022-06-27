3a5a7d708672602aa760509c939f862d7a98de03
Arshak Aghasyan <aghasyanarshak@gmail.com>
2022-06-27 22:57:25 +0400

    add portfolio part

--- my portfolio/js/script.js
+++
@@ -1,8 +0,0 @@
-  // function showBiography() {
-  //   document.querySelector('.some').classList.toggle('show');
-  // }
-  
-  // let clickmenu = document.querySelector('.btn');
-  // clickmenu.addEventListener('click', showBiography);
-
-  
